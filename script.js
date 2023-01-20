d9e66ce593d67cbc2e2086228794277f435c2073
DevCosmo <silentcosmox@gmail.com>
2023-01-20 18:43:47 +0530

    v1.1.2

---
+++ script.js
@@ -0,0 +1,13 @@
+function bcd(val){
+    console.log("Jarvis")
+    document.getElementById("display").value=document.getElementById("display").value+val;
+}
+
+function clr(){
+    document.getElementById("display").value=("")
+}
+function eql(){
+    var text=document.getElementById("display").value
+    var result=eval(text)
+    document.getElementById("display").value=result;
+}
